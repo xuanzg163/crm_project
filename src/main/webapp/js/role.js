7db9d2a253d41c0420e5f9885f155234a0e51a03
xuanzg163 <xuan_zg@163.com>
2018-10-19 15:06:20 +0800

    1、关联权限，前台显示

--- src/main/webapp/js/role.js
+++ src/main/webapp/js/role.js
@@ -16,3 +16,44 @@ function saveOrUpdateRole() {
 function openModifyRoleDialog() {
     openModifyDialog("dg","fm","dlg","更新角色");
 }
+
+//授权
+function openRelationPermissionDialog() {
+    var rows = $("#dg").datagrid("getSelections");
+    if (rows.length == 0){
+        $.messager.alert("来自Crm", "请选择一条数据进行授权");
+        return;
+    }
+    if (rows.length > 1){
+        $.messager.alert("来自Crm", "只能选择一条数据进行授权");
+        return;
+    }
+
+    //打开弹窗
+    $("#permissionDlg").dialog("open");
+    //加载树
+    loadTree(rows[0].id);
+}
+
+var zTreeObj;
+
+function loadTree(roleId) {
+    $.ajax({
+        url: ctx + "/module/queryAllModuleByRoleId?roleId="+roleId,
+        success:function (data) {
+            var setting = {
+                check: {
+                    enable: true,
+                    chkboxType: {"Y": "ps", "N": "ps"}
+                },
+                data: {
+                    simpleData: {
+                        enable: true
+                    }
+                }
+            };
+            var zNodes = data;
+            zTreeObj = $.fn.zTree.init($("#treeDemo"), setting, zNodes);
+        }
+    });
+}
