782153c24e39e76fae662638fd8db36440c1afa8
xuanzg163 <xuan_zg@163.com>
2018-12-03 15:50:23 +0800

    1、添加springMVC拦截器的部分注释，优化代码格式

--- src/main/java/com/shsxt/crm/interceptors/LoginInterceptor.java
+++ src/main/java/com/shsxt/crm/interceptors/LoginInterceptor.java
@@ -23,6 +23,14 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
     @Autowired
     private UserService userService;
 
+    /**
+     *  拦截器方法执行前执行
+     * @param request 请求
+     * @param response 返回
+     * @param handler 预处理方法
+     * @return 布尔值
+     * @throws Exception 异常
+     */
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,
@@ -31,8 +39,10 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
         //获取用户id(cookie)
         Integer userId = LoginUserUtil.releaseUserIdFromCookie(request);
 
-        //校验用户是否登陆
-        AssertUtil.isNotLogin(null == userId || null == userService.queryById(userId), CrmConstant.USER_NOT_LOGIN_MSG);
+        //校验用户是否登陆，想数据库发起查询验证cookie信息中的id对应的数据是否存在
+        AssertUtil.isNotLogin(null == userId ||
+                        null == userService.queryById(userId)
+                , CrmConstant.USER_NOT_LOGIN_MSG);
 
         return true;
     }
